1698aa426ed91828dceea362b6675ede7d298fc0
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-05 12:54:49 +0100

    add player to canvas

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+// canvas.width = 1024;
+// canvas.height = 576;
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "./img/Pellet Town.png";
+
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
+image.onload = () => {
+    c.drawImage(image, -965, -785)
+    c.drawImage(playerImage, 
+        0,
+        0,
+        playerImage.width/4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4)/2, canvas.height/2 - playerImage.height / 2,
+        playerImage.width/4,
+        playerImage.height)
+};
+
+
+
