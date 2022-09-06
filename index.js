43cfd35deaf5305f8ac71195b0fbfee6da914fe0
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-06 13:14:26 +0100

    adding player movement with wasd and arrow keys

--- index.js
+++ index.js
@@ -16,7 +16,44 @@ const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
 image.onload = () => {
-    c.drawImage(image, -965, -785)
+    animate();
+};
+
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+};
+
+const background = new Sprite({
+    position: {x: -1205, y: -900},
+    image: image
+});
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    background.draw();
     c.drawImage(playerImage, 
         0,
         0,
@@ -25,7 +62,78 @@ image.onload = () => {
         canvas.width / 2 - (playerImage.width / 4)/2, canvas.height/2 - playerImage.height / 2,
         playerImage.width/4,
         playerImage.height)
-};
 
+    if(keys.w.pressed && lastKey === 'w') background.position.y+= 2;
+    else if(keys.a.pressed && lastKey === 'a') background.position.x+=2;
+    else if(keys.s.pressed && lastKey === 's') background.position.y-=2;
+    else if(keys.d.pressed && lastKey === 'd') background.position.x-=2;
+}
+animate()
 
+let lastKey = '';
 
+window.addEventListener('keydown', (e) => {
+  
+    switch(e.key){
+        case "Up":
+        case "ArrowUp":
+        case "w":
+        case "W":
+            keys.w.pressed = true;
+            lastKey = 'w';
+            break
+        case "Down":
+        case "ArrowDown":
+        case "s":
+        case "S":
+            keys.s.pressed = true;
+            lastKey = 's';
+            break
+        case "Left":
+        case "ArrowLeft":
+        case "a":
+        case "A":
+            keys.a.pressed = true;
+            lastKey = 'a';
+            break
+        case "Right":
+        case "ArrowRight":
+        case "d":
+        case "D":
+            keys.d.pressed = true;
+            lastKey = 'd';
+            break
+    }
+
+});
+
+window.addEventListener('keyup', (e) => {
+  
+    switch(e.key){
+        case "Up":
+        case "ArrowUp":
+        case "w":
+        case "W":
+            keys.w.pressed = false;
+            break
+        case "Down":
+        case "ArrowDown":
+        case "s":
+        case "S":
+            keys.s.pressed = false;
+            break
+        case "Left":
+        case "ArrowLeft":
+        case "a":
+        case "A":
+            keys.a.pressed = false;
+            break
+        case "Right":
+        case "ArrowRight":
+        case "d":
+        case "D":
+            keys.d.pressed = false;
+            break
+    }
+
+});
